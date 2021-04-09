a84f18935db391ada3653333026f2558b2296639
bhs8574 <bhs8574@gmail.com>
2021-04-09 18:39:47 -0500

    The app should only display the birthday message if it is opened on the correct day now.

--- HappyBirthdayApp/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthdayApp/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        var date = Date()
+        var birthday = Date(2021, 3, 12)
+
+        if (date.month.equals(birthday.month) && date.date.equals(birthday.date)) {
+            setContentView(R.layout.activity_main)
+        }
+        else {
+            setContentView(R.layout.activity_other)
+        }
+
     }
 }
